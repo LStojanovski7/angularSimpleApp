30c16c1e4d62105c369a978de4f3439a8a27f832
LStojanovski7 <lazar.stojanovski@outlook.com>
2021-11-02 12:15:56 +0100

    init

---
+++ angularSimpleApp/src/app/user.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+// import { UserService } from '../user.service'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) {
+
+
+  }
+
+  public getUsers() {
+
+  }
+
+  public createUser() {
+
+  }
+
+  public updateUser() {
+
+  }
+
+  public deleteUser() {
+
+  }
+}
